7f6716cf8b13fc859f7185c98108ff8248491114
Hao <59364085+HAOZHULIN@users.noreply.github.com>
2024-10-23 11:13:14 +0800

    Delete Open-Set-Recognition-master/Ablation/Compare directory

--- Open-Set-Recognition-master/Ablation/Compare/Finetune/supp.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-python3 cifar10_supp.py --mode A
-python3 cifar10_supp.py --mode B
-python3 cifar10_supp.py --mode C
-python3 cifar10_supp.py --mode D
--- Open-Set-Recognition-master/Ablation/Compare/Openness/openness_cifar10.sh
+++
@@ -1,2 +0,0 @@
- python3 cifar10.py --loss SoftmaxLoss
- python3 cifar10.py --loss PSoftmaxLoss
